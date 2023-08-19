2747a9f9f4ea8541f7c24f5b3a63323079a79569
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-08-19 00:44:03 +0000

    end of the excersice

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=pets&api-key=tBtC7uAwz54VpheQMTvgS0rFGF8vaXpR"
+
 function setup(){
+    noCanvas();
+    loadJSON(url, gotData);
 
 }
 
-function draw(){
+function gotData(data){
+
+    let articles = data.response.docs;
+    for (let i = 0; i < articles.length; i++) {
+        createElement('h1', articles[i].headline.main)
+        createP(articles[i].snippet);
+        }
+    print(data.response.docs[1].headline.main);
     
 }
